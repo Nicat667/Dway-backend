0bb1caac17508bc002d48105f8fe28486891b5b5
Nicat667 <nijatvo@code.edu.az>
2026-04-21 17:28:35 +0400

    project created

---
+++ src/main/java/com/dway/dwaybackend/DwayBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dway.dwaybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DwayBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DwayBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dway/dwaybackend/DwayBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dway.dwaybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DwayBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
